cad3b122affba706e19c05091cf4b36abbaeeac8
Damglador <vse.stopchanskyi@gmail.com>
2026-08-14 10:21:26 +0200

    Fix tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,16 +9,16 @@ from xkbeditor import layout
 SAMPLES_DIR = Path(__file__).parent / "symbols"
 
 def testVariantFromFile():
-    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "custom")
+    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "test")
     assert variant1 == parser.getVariantsFromFile(SAMPLES_DIR / "my-layout")[0]
 
 def testVariants():
-    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "custom")
+    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "test")
     variant2 = parser.fromString(variant1.toXkb())[0]
     assert variant1 == variant2, f"Variants don't match!\nVariant1:\n{variant1}\nVariant2:\n{variant2}"
 
 def test_parse_generated_xkb():
-    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "custom")
+    variant1 = parser.getVariantFromFile(SAMPLES_DIR / "my-layout", "test")
     xkb1 = variant1.toXkb()
 
     variant2 = parser.fromString(xkb1)[0]
@@ -29,20 +29,20 @@ def testQuoteSymbols():
     with open(SAMPLES_DIR / "quotes", "r") as file:
         sourceXkb = file.read()
     variant = layout.Variant(
-        id="quotes",
+        id="test",
         keymap={
             "AB08": layout.KeyProps(symbols=["\"", "'", "“", "NoSymbol"])
         }
     )
     xkb = variant.toXkb()
-    assert sourceXkb == xkb, f"""Generated XKB doesn't match with the test sample!
+    assert xkb == sourceXkb, f"""Generated XKB doesn't match with the test sample!
 sample:
 {sourceXkb}
 generated:
 {xkb}"""
 
 def test_toxkb():
-    variant = parser.getVariantFromFile(SAMPLES_DIR / "toxkb-reference", "custom")
+    variant = parser.getVariantFromFile(SAMPLES_DIR / "toxkb-reference", "test")
     xkb = variant.toXkb()
 
     with open(SAMPLES_DIR / "toxkb-reference", "r") as file:
