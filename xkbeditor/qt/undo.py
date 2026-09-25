4203a9f784e177132dc9d3e1dc0c40b10880214b
Damglador <vse.stopchanskyi@gmail.com>
2026-09-25 20:26:58 +0200

    docs: note the bug with FlagsChips not changing

--- xkbeditor/qt/undo.py
+++ xkbeditor/qt/undo.py
@@ -30,6 +30,14 @@ class AddVariant(QUndoCommand):
     def undo(self, /) -> None:
         self.variantList.remove(self.index)
 
+# When the currentIndex is on 0, the VariantList creates an object of Variant for bridge.file,
+# the reference to which is saved here and used in UI. But when current index changes,
+# the UI receives a new instance object of Variant, which is linked to the same data,
+# but since the object of Variant here is different, the UI doesn't receive the update
+# signals from here, which causes the data to update, while UI remains unchanged.
+# Getting from index of VariantList won't work either as it creates a new object
+# of Variant each time. Getting from SymbolsFile each call also doesn't work
+# as it will give variant at current index instead of one that action was performed on.
 class RemoveFlag(QUndoCommand):
     def __init__(self, variant: Variant, flag: str, parent=None):
         super().__init__(parent=parent)
